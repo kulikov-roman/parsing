615d4dcbd51c134f90f19aa124b46b7af66384ca
kulikov-roman <roman-artezio@rambler.ru>
2017-03-28 04:19:01 -0700

    Update parse.py
    Adapt code and create function get_res.
    Commented code after the function get_re.

--- parse.py
+++ parse.py
@@ -1,35 +1,26 @@
 import requests
-import json
-from lxml import html
-url = 'https://www.flyniki.com/en/booking/flight/vacancy.php?'
-DataGet = {}
-DataGet['_ajax[templates][]'] = 'main', 'priceoverview', 'infos', 'flightinfo'
-DataGet['_ajax[requestParams][departure]'] = 'Moscow - Domodedovo'
-DataGet['_ajax[requestParams][destination]'] = 'Cologne/Bonn'
-DataGet['_ajax[requestParams][returnDeparture]'] = ''
-DataGet['_ajax[requestParams][returnDestination]'] = ''
-DataGet['_ajax[requestParams][outboundDate]'] = '2017-05-07'
-DataGet['_ajax[requestParams][returnDate]'] = '2017-05-14'
-DataGet['_ajax[requestParams][adultCount]'] = '1'
-DataGet['_ajax[requestParams][childCount]'] = '0'
-DataGet['_ajax[requestParams][infantCount]'] = '0'
-DataGet['_ajax[requestParams][openDateOverview]'] = ''
-DataGet['_ajax[requestParams][oneway]'] = ''
 
 
-headers={'User-Agent': 'Mozilla/5.0'}
-Data = {}
-Data['departure'] = 'DME'
-Data['outboundDate'] = '2017-05-07'
-Data['returnDate']='2017-05-14'
-Data['oneway'] = '0'
-Data['openDateOverview'] = '0'
-Data['adultCount'] = '1'
-Data['infantCount'] = '0'
+def get_res():
+    url = 'https://www.flyniki.com/en/booking/flight/vacancy.php?'
+    data_res = {'_ajax[templates][]': ('main', 'priceoverview', 'infos', 'flightinfo'),
+                '_ajax[requestParams][departure]': 'DME',
+                '_ajax[requestParams][destination]': 'CGN',
+                '_ajax[requestParams][outboundDate]': '2017-05-07',
+                '_ajax[requestParams][returnDate]': '2017-05-14',
+                '_ajax[requestParams][oneway]': ''}
+    data_req = {'departure': 'DME', 'outboundDate': '2017-05-07',
+                 'returnDate': '2017-05-14',
+                 'oneway': '0',
+                 'adultCount': '1'}
+    ses = requests.Session()
+    ses_req = ses.post(url, data=data_req, verify=False)
+    ses_res = ses.post(ses_req.url, data=data_res, verify=False)
+    return ses_res
+print get_res().text
 
-session = requests.Session()
-request = session.post(url, data=Data, verify=False, headers=headers)
-request = session.post(request.url, data=DataGet, verify=False, headers=headers)
+
+"""
 response = (request.json().get('templates').get('main'))
 tree = html.fromstring(response)
 
@@ -71,4 +62,4 @@ print "Start/end :", (start1[0]), "-", end1[0], "     Duration of journey :", du
 print "Start/end :", (start2[0]), "-", end2[0], "     Duration of journey :", duration_of_journey2[0], "       Economy Classic :", pricesClassic2[0], "     Economy Flex:", pricesFlex2[0]
 print "Start/end :", (start3[0]), "-", end3[0], "     Duration of journey :", duration_of_journey3[0], "       Economy Classic :", pricesClassic3[0], "     Economy Flex:", pricesFlex3[0]
 print "Start/end :", (start4[0]), "-", end4[0], "     Duration of journey :", duration_of_journey4[0], "       Economy Classic :", pricesClassic4[0], "     Economy Flex:", pricesFlex4[0]
-
+"""
