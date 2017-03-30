64c8b3f60803fc2403187a8c006d4858431b48bf
kulikov-roman <roman-artezio@rambler.ru>
2017-03-30 04:05:20 -0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -4,9 +4,9 @@ import argparse
 
 def create_parser():
     parse = argparse.ArgumentParser()
-    parse.add_argument('departure', nargs='?')
-    parse.add_argument('destination', nargs='?')
-    parse.add_argument('outboundDate', nargs='?')
+    parse.add_argument('departure')
+    parse.add_argument('destination')
+    parse.add_argument('outboundDate')
     parse.add_argument('returnDate', nargs='?')
     return parse
 
@@ -24,12 +24,15 @@ def get_res():
                 '_ajax[requestParams][outboundDate]': namespace.outboundDate,
                 '_ajax[requestParams][returnDate]': namespace.returnDate,
                 '_ajax[requestParams][oneway]': ''}
-    data_req = {'departure': 'DME', 'outboundDate': '2017-05-07',
-                'returnDate': '2017-05-14',
-                'oneway': '0',
+    data_req = {'departure': namespace.departure,
+                'outboundDate': namespace.outboundDate,
+                'returnDate': namespace.returnDate,
+                'oneway': '',
                 'adultCount': '1'}
     ses = requests.Session()
     ses_req = ses.post(url, data=data_req, verify=False)
     ses_res = ses.post(ses_req.url, data=data_res, verify=False)
     return ses_res
+
+
 print get_res().text
