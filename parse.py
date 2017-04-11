2cf4e84078ce9f299edbf5d99371fc135c316d8e
kulikov-roman <roman-artezio@rambler.ru>
2017-04-11 22:32:06 +0300

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,37 +1,34 @@
 import argparse
 import sys
-import lxml
-import requests
 import lxml.html
-import datetime
-import re
 
+from datetime import datetime, timedelta
 
-# 13.04.2017
+from requests import Session
 
 
 def scrape():
     parser = create_parser()
     namespace = parser.parse_args()
     validation(namespace)
-    responce = get_res(namespace)
-    parse_responce(responce)
+    res = get_res(namespace)
+    parse_responce(res)
 
 
 def create_parser():
     parse = argparse.ArgumentParser()
     parse.add_argument('departure')
     parse.add_argument('destination')
-    parse.add_argument('outboundDate', type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'))
+    parse.add_argument('outboundDate', type=lambda s: datetime.strptime(s, '%Y-%m-%d'))
     parse.add_argument('returnDate', nargs='?', default='')
     return parse
 
 
 def validation(namespace):
-    date = datetime.datetime.now()
+    date = datetime.now()
     current_date = date.strftime('%Y-%m-%d')
-    # comment magic number
-    limit_date = date + datetime.timedelta(days=360)
+    # the maximum date that can be selected is limited to 360 days from the current date
+    limit_date = date + timedelta(days=360)
     limit_date = limit_date.strftime('%Y-%m-%d')
     if not namespace.departure.isalpha() or len(namespace.departure) != 3:
         print ("The input is not correct. Example, DME")
@@ -68,61 +65,127 @@ def get_res(namespace):
         data_req['oneway'] = 'on'
         data_res['_ajax[requestParams][returnDate]'] = namespace.outboundDate.strftime('%Y-%m-%d')
         data_res['_ajax[requestParams][oneway]'] = 'on'
-    ses = requests.Session()
+    ses = Session()
     ses_req = ses.post(url, data=data_req, verify=False)
     ses_res = ses.post(ses_req.url, data=data_res, verify=False)
     res = (ses_res.json().get('templates').get('main'))
     return res
 
 
-def parse_responce(responce):
-    html = lxml.html.fromstring(responce)
-    outbound_flights = html.xpath('//tbody[@role="radiogroup"]/tr[contains(@class, "flightrow")]')
-
-    # currency = html.xpath('//th[@class="faregrouptoggle ECO style-eco-comf"]/text()')
-    # quotes_list = []
-    for n, flight in enumerate(outbound_flights):
-        price_eco = flight.xpath('./td[contains(@headers, "ECO_COMF")]/label/div[@class="lowest"]/span/text()')
-        price_flex = flight.xpath('./td[contains(@headers, "ECO_PREM")]/label/div[@class="lowest"]/span/text()')
-        price_business = flight.xpath('./td[contains(@headers, "BUS_FLEX")]/label/div[@class="lowest"]/span/text()')
-        print price_eco, price_flex, price_business
-        details_flight = flight.xpath('./../tr[@id="flightDetailsFi_{}"]/td/table/tbody'.format(n))[0]
-        for details in details_flight:
-            time_departure = details.xpath('./td/span/time/text()')
-            flight_numbers = details.xpath('./td[@class="table-text-center"]/text()')
-            city_departure = details.xpath('./td/span/text()')
-            code_city = []
-            flight_number = []
-            for city in city_departure:
-                code = re.findall(r'[A-Z]{3}', city)
-                if code:
-                    code_city.append(code)
-            for number in flight_numbers:
-                if number != " ":
-                    flight_number.append(number)
-            print time_departure, code_city, flight_number
-
-
-"""
-    for flight in outbound_flights:
-        start_end = flight.xpath('./td/span/time/text()')
-        duration = flight.xpath('./td[@class="table-text-left"]/span/text()')
-        prices = flight.xpath('./td[@role="radio"]/label/div[@class="lowest"]/span/text()')
-        prices1 = flight.xpath('//tbody/tr/td/span[@class="notbookable"]/text()')
-        transplant = flight.xpath('//tbody/tr/td/span/text()')
-        number_flight = flight.xpath('//table[@role="presentation"]/tbody/tr/td[@class="table-text-center"]/text()')
-
+def parse_responce(res):
+    html_res = lxml.html.fromstring(res)
+    outbound_flights = html_res.xpath('//div[@class="outbound block"]//tbody/tr[contains(@class, "flightrow")]')
+    return_flights = html_res.xpath('//div[@class="return block"]//tbody/tr[contains(@class, "flightrow")]')
+    currency = html_res.xpath('//th[@class="faregrouptoggle ECO style-eco-comf"]/text()')
+    quotes_list_outbound = []
+    quotes_list_return = []
+    for n, flight_outnbound in enumerate(outbound_flights):
+        price_eco = flight_outnbound.xpath(
+            "./td[contains(@headers, \"ECO_COMF\")]/label/div[@class=\"lowest\"]/span/text()")
+        if price_eco:
+            price_eco = float(price_eco[0])
+        price_flex = flight_outnbound.xpath('./td[contains(@headers, "ECO_PREM")]/label/div['
+                                            '@class="lowest"]/span/text()')
+        if price_flex:
+            price_flex = float(price_flex[0])
+        price_business = flight_outnbound.xpath('./td[contains(@headers, "BUS_FLEX")]//div['
+                                                '@class="lowest"]/span/text()')
+        if price_business:
+            price_business = float(price_business[0])
+        start_end = flight_outnbound.xpath('./td/span/time/text()')
+        duration = flight_outnbound.xpath('./td[@class="table-text-left"]/span/text()')
         quote = {"departure": start_end[0],
                  "arrival": start_end[1],
-                 "segments:{segment_1:{} ,
-                            segment_2:{}}
                  "duration of journey": duration[3],
                  "currency": currency,
-                 "price": {"price_eco": [],
-                           "price_flex": [],
-                           "price_business": []},
+                 "price": {"price_eco": price_eco,
+                           "price_flex": price_flex,
+                           "price_business": price_business},
                  }
-"""
+        quotes_list_outbound.append(quote)
+
+    for n, flight_return in enumerate(return_flights):
+        price_eco = flight_return.xpath(
+            "./td[contains(@headers, \"ECO_COMF\")]/label/div[@class=\"lowest\"]/span/text()")
+        if price_eco:
+            price_eco = float(price_eco[0])
+        price_flex = flight_return.xpath('./td[contains(@headers, "ECO_PREM")]/label/div['
+                                         '@class="lowest"]/span/text()')
+        if price_flex:
+            price_flex = float(price_flex[0])
+        price_business = flight_return.xpath('./td[contains(@headers, "BUS_FLEX")]/label/div['
+                                             '@class="lowest"]/span/text()')
+        if price_business:
+            price_business = float(price_business[0])
+        start_end = flight_outnbound.xpath('./td/span/time/text()')
+        duration = flight_outnbound.xpath('./td[@class="table-text-left"]/span/text()')
+        quote = {"departure": start_end[0],
+                 "arrival": start_end[1],
+                 "duration of journey": duration[3],
+                 "currency": currency,
+                 "price": {"price_eco": price_eco,
+                           "price_flex": price_flex,
+                           "price_business": price_business},
+                 }
+        quotes_list_return.append(quote)
+
+    if not quotes_list_return:
+        print ('                                   {}'.format("outbound flight:"))
+        print (
+            '{}'.format("__________________________________________________________________________________________"))
+        print ('{}   {}   {}   {}   {}   {}'.format('departure',
+                                                    'arrival',
+                                                    'duration of journey',
+                                                    'Economy Classic',
+                                                    'Economy Flex',
+                                                    'Business Flex'))
+        for quote_list in quotes_list_outbound:
+            print ('  {}      {}      {}            {}            {}            {}'.format(quote_list["departure"],
+                                                                                           quote_list['arrival'],
+                                                                                           quote_list[
+                                                                                               "duration of journey"],
+                                                                                           quote_list["price"][
+                                                                                               "price_eco"],
+                                                                                           quote_list["price"][
+                                                                                               "price_flex"],
+                                                                                           quote_list["price"][
+                                                                                               "price_business"]))
+    else:
+        print ('                                   {}'.format("outbound flight:"))
+        print (
+            '{}'.format("__________________________________________________________________________________________"))
+        print ('{}   {}   {}   {}   {}   {}'.format('departure',
+                                                    'arrival',
+                                                    'duration of journey',
+                                                    'Economy Classic',
+                                                    'Economy Flex',
+                                                    'Business Flex'))
+        for quote_list in quotes_list_outbound:
+            print ('  {}      {}      {}            {}            {}            {}'.format(quote_list["departure"],
+                                                                                           quote_list['arrival'],
+                                                                                           quote_list[
+                                                                                               "duration of journey"],
+                                                                                           quote_list["price"][
+                                                                                               "price_eco"],
+                                                                                           quote_list["price"][
+                                                                                               "price_flex"],
+                                                                                           quote_list["price"][
+                                                                                               "price_business"]))
+        print ('                                   {}'.format("return flight:"))
+        print (
+            '{}'.format("__________________________________________________________________________________________"))
+        for quote_list in quotes_list_return:
+            print ('  {}      {}      {}            {}            {}            {}'.format(quote_list["departure"],
+                                                                                           quote_list['arrival'],
+                                                                                           quote_list[
+                                                                                               "duration of journey"],
+                                                                                           quote_list["price"][
+                                                                                               "price_eco"],
+                                                                                           quote_list["price"][
+                                                                                               "price_flex"],
+                                                                                           quote_list["price"][
+                                                                                               "price_business"]))
+
 
 if __name__ == '__main__':
     scrape()
