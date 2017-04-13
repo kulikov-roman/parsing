1c9d026d5055a39bd042bcb5c74ea56369e44a18
kulikov-roman <roman-artezio@rambler.ru>
2017-04-13 10:16:23 -0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -7,6 +7,15 @@ import lxml.html
 from datetime import datetime, timedelta
 import requests
 
+from time import sleep
+
+
+CLASS_MAPPING = {
+    'ECO_COMF': 'Economy Classic',
+    'ECO_PREM': 'Economy Flex',
+    'BUS_FLEX': 'Business Flex'
+}
+
 
 def scrape():
     parser = create_parser()
@@ -81,37 +90,66 @@ def get_res(namespace):
 
 
 def parse_responce(res):
-    html_res = lxml.html.fromstring(res)
-    outbound_flights = html_res.xpath('//div[@class="outbound block"]//tbody/tr[contains(@class, "flightrow")]')
-    return_flights = html_res.xpath('//div[@class="return block"]//tbody/tr[contains(@class, "flightrow")]')
-    currency = html_res.xpath('//th[@class="faregrouptoggle ECO style-eco-comf"]/text()')
-    quotes_list_outbound = []
-    quotes_list_return = []
-    quotes_sum = []
-    for n, flight_outnbound in enumerate(outbound_flights):
-        price_eco = flight_outnbound.xpath(
-            "./td[contains(@headers, \"ECO_COMF\")]/label/div[@class=\"lowest\"]/span/text()")
-        if price_eco:
-            price_eco = float(price_eco[0])
-        price_flex = flight_outnbound.xpath('./td[contains(@headers, "ECO_PREM")]/label/div['
-                                            '@class="lowest"]/span/text()')
-        if price_flex:
-            price_flex = float(price_flex[0])
-        price_business = flight_outnbound.xpath('./td[contains(@headers, "BUS_FLEX")]//div['
-                                                '@class="lowest"]/span/text()')
-        if price_business:
-            price_business = float(price_business[0])
-        start_end = flight_outnbound.xpath('./td/span/time/text()')
-        duration = flight_outnbound.xpath('./td[@class="table-text-left"]/span/text()')
-        quote = {"departure": start_end[0],
-                 "arrival": start_end[1],
-                 "duration of journey": duration[3],
-                 "currency": currency,
-                 "price": {"price_eco": price_eco,
-                           "price_flex": price_flex,
-                           "price_business": price_business}, }
-        quotes_list_outbound.append(quote)
+    quotes = []
 
+    html = lxml.html.fromstring(res)
+    outbound_list = html.xpath('//div[@class="outbound block"]//tbody/tr[contains(@class, "flightrow")]')
+    return_list = html.xpath('//div[@class="return block"]//tbody/tr[contains(@class, "flightrow")]')
+
+    currency = list(set(html.xpath('//th[contains(@class, "faregrouptoggle")]/text()')))[0]
+    for out_info in outbound_list:
+        quote = {}
+        outbound_fares = out_info.xpath(".//td[contains(@class, 'fare') and not(./span[@class='notbookable'])]")
+        for out_fare in outbound_fares:
+            start_end = out_info.xpath('./td/span/time/text()')
+            duration = out_info.xpath('./td[@class="table-text-left"]'
+                                      '/span[contains(@id, "flightDurationFi")]/text()')[0]
+            price = out_fare.xpath('.//div[@class="lowest"]'
+                                   '/span[contains(@id, "price")]/text()')[0]
+            outbound_leg = {
+                "departure": start_end[0],
+                "arrival": start_end[1],
+                "flight_duration": duration,
+                "currency": currency,
+                "price": float(price),
+                "class": [CLASS_MAPPING[x] for x in CLASS_MAPPING.keys()
+                          if x in out_fare.xpath('./@class')[0]][0]
+                }
+            quote['outbound_leg'] = outbound_leg
+            # quote['total_price'] = outbound_leg['price']
+            print outbound_leg['price']
+
+            if return_list:
+                for in_info in return_list:
+                    inbound_fares = in_info.xpath(
+                        ".//td[contains(@class, 'fare') and not(./span[@class='notbookable'])]")
+                    for in_fare in inbound_fares:
+                        start_end = in_info.xpath('./td/span/time/text()')
+                        duration = in_info.xpath('./td[@class="table-text-left"]'
+                                                 '/span[contains(@id, "flightDurationFi")]/text()')[0]
+                        price = in_fare.xpath('.//div[@class="lowest"]'
+                                              '/span[contains(@id, "price")]/text()')
+                        # print "="*5, price, "="*5
+                        inbound_leg = {
+                            "departure": start_end[0],
+                            "arrival": start_end[1],
+                            "flight_duration": duration,
+                            "currency": currency,
+                            "price": float(price[0]),
+                            "class": [CLASS_MAPPING[x] for x in CLASS_MAPPING.keys()
+                                      if x in in_fare.xpath('./@class')[0]][0]
+                        }
+                        quote['inbound_leg'] = inbound_leg
+                        # total_price = outbound_leg['price'] + inbound_leg['price']
+                        quote['total_price'] = outbound_leg['price'] + inbound_leg['price']
+                        quotes.append(quote)
+            else:
+                quote['total_price'] = outbound_leg['price']
+                quotes.append(quote)
+
+    for i in outbound_list:
+        print i
+    """
     for n, flight_return in enumerate(return_flights):
         price_eco = flight_return.xpath("./td[contains(@headers, \"ECO_COMF\")]/label/div["
                                         "@class=\"lowest\"]/span/text()")
@@ -165,39 +203,64 @@ def parse_responce(res):
     else:
         for quote_list_outbound in quotes_list_outbound:
             for quote_list_return in quotes_list_return:
-                sum_classic = 0
-                sum_flex = 0
-                sum_business = 0
-                quotes = {'departure_outbound': quote_list_return['departure'],
-                          'arrival_outbound': quote_list_return['arrival'],
-                          'duration_of_journey_outbound': quote_list_return['duration of journey'],
-                          'price_outbound': {'price_eco': quote_list_return['price']['price_eco'],
-                                             'price_flex': quote_list_return['price']['price_flex'],
-                                             'business_flex': quote_list_return['price']['price_business']},
-                          'departure_return': quote_list_outbound['departure'],
-                          'arrival_return': quote_list_outbound['arrival'],
-                          'duration_of_journey_return': quote_list_outbound['duration of journey'],
+                amount = 0
+
+                quotes = {'departure_return': quote_list_return['departure'],
+                          'arrival_return': quote_list_return['arrival'],
+                          'duration_of_journey_return': quote_list_return['duration of journey'],
+                          'price_return': {'price_eco': quote_list_return['price']['price_eco'],
+                                           'price_flex': quote_list_return['price']['price_flex'],
+                                           'business_flex': quote_list_return['price']['price_business']},
+                          'departure_outbound': quote_list_outbound['departure'],
+                          'arrival_outbound': quote_list_outbound['arrival'],
+                          'duration_of_journey_outbound': quote_list_outbound['duration of journey'],
                           'currency': quote_list_outbound['currency'],
-                          'price_return': {'price_eco': quote_list_outbound['price']['price_eco'],
-                                           'price_flex': quote_list_outbound['price']['price_flex'],
-                                           'business_flex': quote_list_outbound['price']['price_business']},
-                          'amount_classic': sum_classic,
-                          'amount_flex': sum_flex,
-                          'amount_business': sum_business}
+                          'price_outbound': {'price_eco': quote_list_outbound['price']['price_eco'],
+                                             'price_flex': quote_list_outbound['price']['price_flex'],
+                                             'business_flex': quote_list_outbound['price']['price_business']},
+                          'amount': amount}
+
                 if quote_list_return['price']['price_eco'] and quote_list_outbound['price']['price_eco']:
-                    sum_classic = quote_list_return['price']['price_eco'] + quote_list_outbound['price']['price_eco']
-                    quotes['amount_classic'] = sum_classic
+                    amount = quote_list_return['price']['price_eco'] + quote_list_outbound['price']['price_eco']
+                    quotes['amount'] = amount
+                    quotes_sum.append(quotes)
+                if quote_list_return['price']['price_eco'] and quote_list_outbound['price']['price_flex']:
+                    amount = quote_list_return['price']['price_eco'] + quote_list_outbound['price']['price_flex']
+                    quotes['amount'] = amount
+                    quotes_sum.append(quotes)
+                if quote_list_return['price']['price_eco'] and quote_list_outbound['price']['price_business']:
+                    amount = quote_list_return['price']['price_eco'] + quote_list_outbound['price']['price_business']
+                    quotes['amount'] = amount
+                    quotes_sum.append(quotes)
+                if quote_list_return['price']['price_flex'] and quote_list_outbound['price']['price_eco']:
+                    amount = quote_list_return['price']['price_eco'] + quote_list_outbound['price']['price_eco']
+                    quotes['amount'] = amount
+                    quotes_sum.append(quotes)
                 if quote_list_return['price']['price_flex'] and quote_list_outbound['price']['price_flex']:
-                    sum_flex = quote_list_return['price']['price_flex'] + quote_list_outbound['price']['price_flex']
-                    quotes['amount_flex'] = sum_flex
+                    amount = quote_list_return['price']['price_eco'] + quote_list_outbound['price']['price_flex']
+                    quotes['amount'] = amount
+                    quotes_sum.append(quotes)
+                if quote_list_return['price']['price_flex'] and quote_list_outbound['price']['price_business']:
+                    amount = quote_list_return['price']['price_eco'] + quote_list_outbound['price']['price_business']
+                    quotes['amount'] = amount
+                    quotes_sum.append(quotes)
+                if quote_list_return['price']['price_business'] and quote_list_outbound['price']['price_eco']:
+                    amount = quote_list_return['price']['price_business'] + quote_list_outbound['price']['price_eco']
+                    quotes['amount'] = amount
+                    quotes_sum.append(quotes)
+                if quote_list_return['price']['price_business'] and quote_list_outbound['price']['price_flex']:
+                    amount = quote_list_return['price']['price_business'] + quote_list_outbound['price']['price_flex']
+                    quotes['amount'] = amount
+                    quotes_sum.append(quotes)
                 if quote_list_return['price']['price_business'] and quote_list_outbound['price']['price_business']:
-                    price_business_outbound = quote_list_outbound['price']['price_business']
-                    price_business_return = quote_list_return['price']['price_business']
-                    sum_business = price_business_outbound + price_business_return
-                    quotes['amount_business'] = sum_business
-                quotes_sum.append(quotes)
-        sorted(quotes_sum, 'amount_classic')
-        for quote_sum in quotes_sum:
+                    amount = quote_list_return['price']['price_business'] + quote_list_outbound['price'][
+                        'price_business']
+                    quotes['amount'] = amount
+                    quotes_sum.append(quotes)
+        for i in quotes_sum:
+            print i
+
+        for quote_sum in sorted(quotes_sum, key=lambda d: d['amount_classic']):
             table = texttable.Texttable()
             table.set_cols_dtype(['t', 't', 't', 'a', 'a', 'a', 'a'])
             table.set_cols_align(["c", "c", "c", "c", "c", "c", "c"])
@@ -231,9 +294,8 @@ def parse_responce(res):
                              quote_sum["amount_business"],
                              quote_sum["currency"][0]]])
             print table.draw() + "\n"
+        """
 
 
 if __name__ == '__main__':
     scrape()
-
-
